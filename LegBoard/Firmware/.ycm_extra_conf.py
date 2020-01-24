4499d9b4cbe7c9fdef5b3ad4b3a7c49e4c143623
contradict <contradict@gmail.com>
2020-01-24 15:34:47 -0800

    Add skeleton for LegBoard firmware
    Move README into new LegBoard directory
    Add template for STM32F277 project

---
+++ LegBoard/Firmware/.ycm_extra_conf.py
@@ -0,0 +1,68 @@
+import os.path as p
+
+DIR_OF_THIS_SCRIPT = p.abspath(p.dirname(__file__))
+
+BASE_FLAGS = [
+    "-Wall",
+    "-Wextra",
+    "-Werror",
+    "-Wno-long-long",
+    # '-Wno-variadic-macros',
+    "-fexceptions",
+    "-ferror-limit=10000",
+    # '-DNDEBUG',
+    "-std=c++11",
+    "-xc++",
+    "-mthumb",
+    "-DSTM32F722xx",
+    "-mmcu=cortex-m7",
+    "-mthumb",
+    "-mfpu=fpv5-sp-d16",
+    "-mfloat-abi=hard",
+    "--sysroot",
+    "/usr/lib/gcc/arm-none-eabi",
+    "-isystem",
+    "/usr/include/newlib/c++/6.3.1/arm-none-eabi",
+    "-isystem",
+    "/usr/include/newlib/",
+    "-isystem",
+    "/usr/lib/gcc/arm-none-eabi/6.3.1/include",
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/Core/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/Device/ST/STM32F7xx/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/DSP/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/NN/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/RTOS/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/CMSIS/RTOS2/Include"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "Drivers/STM32F7xx_HAL_Driver/Inc"),
+    "-I",
+    p.join(
+        DIR_OF_THIS_SCRIPT,
+        "Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM7/r0p1/"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT,
+           "Middlewares/Third_Party/FreeRTOS/Source/include/"),
+    "-I",
+    p.join(DIR_OF_THIS_SCRIPT, "inc"),
+]
+
+SOURCE_EXTENSIONS = [
+    ".cpp",
+    ".cxx",
+    ".cc",
+    ".c",
+]
+
+HEADER_EXTENSIONS = [".h", ".hxx", ".hpp", ".hh"]
+
+
+def Settings(**kwargs):
+    return {
+        "flags": BASE_FLAGS,
+    }
