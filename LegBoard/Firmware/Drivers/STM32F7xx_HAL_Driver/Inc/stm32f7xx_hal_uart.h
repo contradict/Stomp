b5787b8a092741b1fde174740e04379251b234c0
contradict <contradict@gmail.com>
2020-02-27 23:33:14 -0800

    Add UART Receive timeout to HAL

--- LegBoard/Firmware/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h
+++ LegBoard/Firmware/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h
@@ -1354,6 +1354,7 @@ void HAL_UART_IRQHandler(UART_HandleTypeDef *huart);
 void HAL_UART_TxHalfCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart);
+void HAL_UART_RxToCallback(UART_HandleTypeDef *huart);
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart);
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart);
 void HAL_UART_AbortCpltCallback(UART_HandleTypeDef *huart);
--- LegBoard/Firmware/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c
+++ LegBoard/Firmware/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_uart.c
@@ -2113,9 +2113,9 @@ void HAL_UART_IRQHandler(UART_HandleTypeDef *huart)
   errorflags = (isrflags & (uint32_t)(USART_ISR_PE | USART_ISR_FE | USART_ISR_ORE | USART_ISR_NE));
   if (errorflags == 0U)
   {
-    /* UART in mode Receiver ---------------------------------------------------*/
+    /* UART in mode Receiver, character received -------------------------------*/
     if (((isrflags & USART_ISR_RXNE) != 0U)
-        && ((cr1its & USART_CR1_RXNEIE) != 0U))
+         && ((cr1its & USART_CR1_RXNEIE) != 0U))
     {
       if (huart->RxISR != NULL)
       {
@@ -2123,7 +2123,33 @@ void HAL_UART_IRQHandler(UART_HandleTypeDef *huart)
       }
       return;
     }
-  }
+    /* UART in mode Receiver, receive timeout ----------------------------------*/
+    else if((((isrflags & USART_ISR_RTOF) != 0U)
+            && ((cr1its & USART_CR1_RTOIE) != 0U)))
+    {
+      SET_BIT(huart->Instance->ICR, USART_ICR_RTOCF);
+      /* Disable the UART Parity Error Interrupt, Timeout and RXNE interrupts */
+      CLEAR_BIT(huart->Instance->CR1, (USART_CR1_RXNEIE | USART_CR1_PEIE | USART_CR1_RTOIE));
+
+      /* Disable the UART Error Interrupt: (Frame error, noise error, overrun error) */
+      CLEAR_BIT(huart->Instance->CR3, USART_CR3_EIE);
+
+      /* Rx process is completed, restore huart->RxState to Ready */
+      huart->RxState = HAL_UART_STATE_READY;
+
+      /* Clear RxISR function pointer */
+      huart->RxISR = NULL;
+
+#if (USE_HAL_UART_REGISTER_CALLBACKS == 1)
+      /*Call registered Rx complete callback*/
+      huart->RxToCallback(huart);
+#else
+      /*Call legacy weak Rx complete callback*/
+      HAL_UART_RxToCallback(huart);
+#endif /* USE_HAL_UART_REGISTER_CALLBACKS */
+
+    }
+ }
 
   /* If some errors occur */
   if ((errorflags != 0U)
@@ -2268,6 +2294,7 @@ void HAL_UART_IRQHandler(UART_HandleTypeDef *huart)
     return;
   }
 
+
 }
 
 /**
@@ -2329,6 +2356,17 @@ __weak void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
            the HAL_UART_RxHalfCpltCallback can be implemented in the user file.
    */
 }
+
+__weak void HAL_UART_RxToCallback(UART_HandleTypeDef *huart)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(huart);
+
+  /* NOTE: This function should not be modified, when the callback is needed,
+           the HAL_UART_RxHalfCpltCallback can be implemented in the user file.
+   */
+}
+
 
 /**
   * @brief  UART error callback.
@@ -3367,6 +3405,7 @@ static void UART_RxISR_8BIT(UART_HandleTypeDef *huart)
   /* Check that a Rx process is ongoing */
   if (huart->RxState == HAL_UART_STATE_BUSY_RX)
   {
+
     uhdata = (uint16_t) READ_REG(huart->Instance->RDR);
     *huart->pRxBuffPtr = (uint8_t)(uhdata & (uint8_t)uhMask);
     huart->pRxBuffPtr++;
