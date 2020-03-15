04ba7188287a9cb804071eb53bf2c364d666f402
contradict <contradict@gmail.com>
2020-03-14 19:25:44 -0700

    Use measured voltage ranges for angle sensors

--- LegBoard/Firmware/src/linearize_feedback.c
+++ LegBoard/Firmware/src/linearize_feedback.c
@@ -82,13 +82,13 @@ struct SensorCalibration {
 static struct SensorCalibration calibration_constants __attribute__ ((section (".storage.linearize"))) = {
     .theta_offset = {
       //THETA_OFFSET(Vmin,     Vmax,                 Thetamin,                 Thetamax)
-        THETA_OFFSET(0.916f, 4.774f, -M_PI_2-25.1*M_PI/180.0f, -M_PI_2+25.1*M_PI/180.0f),  // CURL
-        THETA_OFFSET(0.225f, 4.768f,               -M_PI/8.0f,                M_PI/8.0f),  // SWING
-        THETA_OFFSET(1.009f, 3.637f,          -13*M_PI/180.0f,           13*M_PI/180.0f)}, // LIFT
+        THETA_OFFSET(0.900f, 4.800f, -M_PI_2-25.1*M_PI/180.0f, -M_PI_2+25.1*M_PI/180.0f),  // CURL
+        THETA_OFFSET(0.250f, 4.800f,               -M_PI/8.0f,                M_PI/8.0f),  // SWING
+        THETA_OFFSET(1.016f, 3.660f,          -13*M_PI/180.0f,           13*M_PI/180.0f)}, // LIFT
     .theta_scale = {
-        THETA_SCALE( 0.916f, 4.774f, -M_PI_2-25.1*M_PI/180.0f, -M_PI_2+25.1*M_PI/180.0f),  // CURL
-        THETA_SCALE( 0.225f, 4.768f,               -M_PI/8.0f,                M_PI/8.0f),  // SWING
-        THETA_SCALE( 1.009f, 3.637f,          -13*M_PI/180.0f,          13*M_PI/180.0f)},  // LIFT
+        THETA_SCALE( 0.900f, 4.800f, -M_PI_2-25.1*M_PI/180.0f, -M_PI_2+25.1*M_PI/180.0f),  // CURL
+        THETA_SCALE( 0.250f, 4.800f,               -M_PI/8.0f,                M_PI/8.0f),  // SWING
+        THETA_SCALE( 1.016f, 3.660f,          -13*M_PI/180.0f,          13*M_PI/180.0f)},  // LIFT
     .joint_min = {
         5.8, // CURL
         1.080, // SWING
