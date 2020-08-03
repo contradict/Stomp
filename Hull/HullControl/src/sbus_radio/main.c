77ca18a4137f23b72c4f7b3a666b364d3b91efc2
Jascha Little <jtlittle@gmail.com>
2020-08-02 20:59:47 -0700

    Test opening ttyS1

--- Hull/HullControl/src/sbus_radio/main.c
+++ Hull/HullControl/src/sbus_radio/main.c
@@ -1,4 +1,10 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <lcm/lcm.h>
 
@@ -15,6 +21,17 @@ int main(int argc, char **argv)
     }
 
     stomp_control_radio radio_message;
+
+    printf("Trying to open UART10\n");
+
+    int serial_port;
+    serial_port = open("/dev/ttyS1", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (serial_port < 0)
+    {
+        printf("Error %i from open: %s\n", errno, strerror(errno));
+    } else {
+        printf("No error while opening port\n");
+    }
 
     while(true)
     {
