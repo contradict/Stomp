d9fee5d9b810de322d3fe9b701fef106391ccd41
contradict <contradict@gmail.com>
2020-08-18 01:53:55 +0000

    Add setting gain to a non-zero value

--- Hull/HullControl/src/leg_control/test/test_gain_set.c
+++ Hull/HullControl/src/leg_control/test/test_gain_set.c
@@ -56,15 +56,25 @@ int main(int argc, char** argv)
     float zero_gain[3] = {0.0f, 0.0f, 0.0f};
     float zero_damping[3] = {0.0f, 0.0f, 0.0f};
 
+    float real_gain[3] = {15.0f, 15.0f, 15.0f};
+    float real_damping[3] = {2.0f, 2.0f, 2.0f};
+
+
     int loop_count=0;
     while(1)
     {
         int err = set_servo_gains(ctx, address, &zero_gain, &zero_damping);
         if(err == -1)
         {
-            logm(SL4C_ERROR, "Unable to set gains for leg 0x%02x.", address);
+            logm(SL4C_ERROR, "Unable to zero gains for leg 0x%02x.", address);
         }
         struct timespec req = {.tv_sec=0, .tv_nsec=period*1000000};
+        nanosleep(&req, NULL);
+        err = set_servo_gains(ctx, address, &real_gain, &real_damping);
+        if(err == -1)
+        {
+            logm(SL4C_ERROR, "Unable to set gains for leg 0x%02x.", address);
+        }
         nanosleep(&req, NULL);
         logm(SL4C_INFO, "Loop %d.", loop_count);
         loop_count++;
