305927faae924332c06b9ac643f3d9677a432a36
Jascha Little <jtlittle@gmail.com>
2020-09-10 00:37:08 -0700

    Clean up packet def, and create matching structs

--- Hull/HullControl/src/telemetry/messages.h
+++ Hull/HullControl/src/telemetry/messages.h
@@ -16,11 +16,36 @@
 // In a better world there would be autogeneration to eliminate this pitfall.
 typedef enum msg_id {
     INVALID = 0,  // Avoid having a meaningful ID of 0
+    GNRL,
+    LEGS,
     TANK_PSI,
     HIGHEST_ID
 } msg_id;
 // Our msg_id has to be safely castable to a uint8_t
 static_assert(HIGHEST_ID <= 256, "Too many messages!");
+
+struct gnrl {
+    uint8_t tank_psi;
+    uint8_t rail_psi;
+    uint8_t flags_byte;
+//  unsigned int sbus_failsafe : 1;
+//  unsigned int sbus_no_data : 1;
+} __attribute__((__packed__));
+
+struct legs {
+    uint8_t curl_base_psi[6];
+    uint8_t curl_rod_psi[6];
+    uint8_t swing_base_psi[6];
+    uint8_t swing_rod_psi[6];
+    uint8_t lift_base_psi[6];
+    uint8_t lift_rod_psi[6];
+    uint8_t toe_pos_x[6];
+    uint8_t toe_cmd_x[6];
+    uint8_t toe_pos_y[6];
+    uint8_t toe_cmd_y[6];
+    uint8_t toe_pos_z[6];
+    uint8_t toe_cmd_z[6];
+} __attribute__((__packed__));
 
 struct tank_psi {
     uint32_t psi;
