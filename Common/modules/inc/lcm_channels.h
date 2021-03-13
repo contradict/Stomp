290457c457a80b318e6ddf922fbead5568ebcf95
CRATE <crate@crate-tach.localdomain>
2021-03-13 19:15:25 +0000

    initial take on moving some code to top-level Common directory to share between Turret and Hull

---
+++ Common/modules/inc/lcm_channels.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const char* SBUS_RADIO_COMMAND="SBUS_COMMAND";
---
+++ Turret/TurretControl/inc/turret_control.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+#include <lcm/lcm.h>
+#include <toml.h>
+
