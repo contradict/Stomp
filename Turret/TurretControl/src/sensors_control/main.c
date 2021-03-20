1fc3d43184ac93fdd169ee3ff4860dc4fca4dfba
Matthew Scot <mjs@scottopia.net>
2021-03-20 11:34:12 -0700

    Starting sensors_control/  Transfering this work to BBAI

--- Turret/TurretControl/src/sensors_control/main.c
+++ Turret/TurretControl/src/sensors_control/main.c
@@ -13,6 +13,11 @@
 #include "lcm/stomp_control_radio.h"
 #include "lcm/stomp_sensors_control.h"
 
+static const string s_hammer_angle_sensor_device = "/sys/bus/iio/devices/iio:device0/in_voltage1_raw";
+static const string s_turret_angle_sensor_device = "/sys/bus/iio/devices/iio:device0/in_voltage2_raw";
+static const string s_throw_pressure_sensor_device = "/sys/bus/iio/devices/iio:device0/in_voltage3_raw";
+static const string s_retract_pressure_sensor_device = "/sys/bus/iio/devices/iio:device0/in_voltage4_raw";
+
 int main(int argc, char **argv)
 {
     int opt;
@@ -33,24 +38,51 @@ int main(int argc, char **argv)
         logm(SL4C_FATAL, "Failed to initialize LCM.\n");
         exit(2);
     }
+    stomp_sensors_control lcm_msg;
+
+    // open each of the analog devices
+
+    int hammer_angle_fd;
+    hammer_angle_fd = open(s_hammer_angle_sensor_device, O_RDWR);
+
+    if (hammer_angle_fd < 0)
+    {
+        logm(SL4C_FATAL, "Error %i from open(): %s", errno, strerror(errno));
+        return 1;
+    } 
+
+    int turret_angle_fd;
+    turret_angle_fd = open(s_turret_angle_sensor_device, O_RDWR);
+
+    if (turret_angle_fd < 0)
+    {
+        logm(SL4C_FATAL, "Error %i from open(): %s", errno, strerror(errno));
+        return 1;
+    } 
+
+    int throw_pressure_fd;
+    throw_pressure_fd = open(s_throw_pressure_sensor_device, O_RDWR);
+
+    if (throw_pressure_fd < 0)
+    {
+        logm(SL4C_FATAL, "Error %i from open(): %s", errno, strerror(errno));
+        return 1;
+    } 
+
+    int retract_pressure_fd;
+    retract_pressure_fd = open(s_retract_pressure_sensor_device, O_RDWR);
+
+    if (retract_pressure_fd < 0)
+    {
+        logm(SL4C_FATAL, "Error %i from open(): %s", errno, strerror(errno));
+        return 1;
+    } 
+
+    // Read Sensor Inputs, process, and then publish to LCM
 
     while(true)
     {
-        int lcm_fd = lcm_get_fileno(lcm);
-        fd_set fds;
-        FD_ZERO(&fds);
-        FD_SET(lcm_fd, &fds);
-
-        struct timeval timeout = {
-            0,
-            10000,
-        };
-
-        int status = select(lcm_fd + 1, &fds, 0, 0, &timeout);
-        if(status == 1 && FD_ISSET(lcm_fd, &fds))
-        {
-            lcm_handle(lcm);
-        }
+        stomp_sensors_controller_publish(lcm, SENSORS_CONTROL, &lcm_msg);
     }
 
     lcm_destroy(lcm);
