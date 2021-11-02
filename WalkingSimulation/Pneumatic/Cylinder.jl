10c4604e71a1595966622a3830d77eb86203d17d
contradict <contradict@gmail.com>
2021-11-02 10:43:24 -0700

    ComponentArrays implementation.

--- WalkingSimulation/Pneumatic/Cylinder.jl
+++ WalkingSimulation/Pneumatic/Cylinder.jl
@@ -1,15 +1,11 @@
 using DifferentialEquations, CSV, Unitful, Parameters
 using RecursiveArrayTools
+using ComponentArrays
 using Plots, UnitfulRecipes
 using Optim
 
-const γ = 1.4 # = cₚ / cᵥ for air
 
-const R = 8.31446261815324u"J/mol/K" # Ideal gas constant, [J/(mol K)]
-
-const Rₛ = 287.058u"J/kg/K"   # Specific gas constant for dry air, R / (molar mass), [J/(kg K)]
-
-const Patmospheric = 101325.0u"Pa"  # Pa
+# const R = 8.31446261815324u"J/mol/K" # Ideal gas constant, [J/(mol K)]
 
 include("valve.jl")
 
@@ -20,18 +16,20 @@ const bestValve = ISO6358Valve(;C=3.31e-8u"m^4*s*kg^-1", bcᵣ=0.527)
 # https://www.researchgate.net/publication/238185949_Heat_transfer_evaluation_of_industrial_pneumatic_cylinders
 function cylinder!(du, u, p, t)
 
-    L, Apiston, Cpiston, r, Vdb, Amax, Tamb, λ, Mass, Tin, σs, σd, vmin, Pin, Patm, Tatm, command, valve = p
+    L, Rp, r, Vdb, Amax, Tamb, λ, Mass, Tin, σs, σd, vmin, Pin, Patm, Tatm, command, valve, γ, Rₛ = p
     x, v, Pb, Tb, Pr, Tr = u
 
     # force on base - force on rod face of piston - force on rod end
+    Apiston = π*Rp^2
     force = Pb * Apiston - Pr * r * Apiston - Apiston * (1-r) * Patm
 
     du[1] = v
-    du[2] = (force - σd*sign(v/1u"m/s")) / Mass
+    du[2] = (force - σd*sign(v/oneunit(v))) / Mass
 
     # [kg] [m] [s]^-2 [kg] [m]^-2 [m]^-4 = [kg]^2 [m]^-5 [s]^-2
     Vpiston = Apiston * x
     dVpiston = Apiston * v
+    Cpiston = 2π*Rp
     # Eqn 1 at base side of piston
     du[3] = (-γ * Pb / Vpiston * dVpiston +
               γ * Rₛ / Vpiston * valveFlow(command, Pb, Tb, Tb, Pin, Tin, Tin, Patm, Tatm, Tatm, Amax, Vdb, valve) +
@@ -54,18 +52,55 @@ function cylinder!(du, u, p, t)
     du
 end
 
+function compcylinder!(du, u, p, t)
+
+    # force on base - force on rod face of piston - force on rod end
+    Apiston = π*p.Rp^2
+    force = u.Pb * Apiston - u.Pr * p.r * Apiston - Apiston * (1-p.r) * p.Patm
+
+    du[1] = u.v
+    du[2] = (force - p.σd*sign(u.v/oneunit(u.v))) / p.Mass
+
+    # [kg] [m] [s]^-2 [kg] [m]^-2 [m]^-4 = [kg]^2 [m]^-5 [s]^-2
+    Vpiston = Apiston * u.x
+    dVpiston = Apiston * u.v
+    Cpiston = 2π*p.Rp
+    # Eqn 1 at base side of piston
+    du[3] = (-p.γ * u.Pb / Vpiston * dVpiston +
+             p.γ * p.Rₛ / Vpiston * valveFlow(p.command, u.Pb, u.Tb, u.Tb, p.Pin, p.Tin, p.Tin, p.Patm, p.Tatm, p.Tatm, p.Amax, p.Vdb, p.valve[1]) +
+              (p.γ - 1) / Vpiston * p.λ * (u.x * Cpiston) * (p.Ta - u.Tb))
+    # Eqn 2 at base side of piston
+    du[4] = (u.Tb / Vpiston * dVpiston * (1 - p.γ) +
+             p.Rₛ * u.Tb / Vpiston / u.Pb * valveFlow(p.command, u.Pb, u.Tr, u.Tr*(p.γ-1), p.Pin, p.Tin, (p.γ*p.Tin - u.Tr), p.Patm, p.Tatm, (p.γ*p.Tatm-u.Tr), p.Amax, p.Vdb, p.valve[1]) +
+             (p.γ - 1) * u.Tb / u.Pb / Vpiston * p.λ * (u.x * Cpiston) * (p.Ta - u.Tb))
+
+    Vrod = Apiston * p.r * (p.L - u.x)
+    dVrod = Apiston * p.r * -u.v
+    # Eqn 1 at rod side of piston
+    du[5] = (-p.γ * u.Pr / Vrod * dVrod +
+             p.γ * p.Rₛ / Vrod * valveFlow(-p.command, u.Pr, u.Tr, u.Tr, p.Pin, p.Tin, p.Tin, p.Patm, p.Tatm, p.Tatm, p.Amax, p.Vdb, p.valve[1]) +
+              (p.γ - 1) / Vrod * p.λ * ((p.L-u.x) * Cpiston) * (p.Ta - u.Tr))
+    # Eqn 2 at rod side of piston
+    du[6] = (u.Tr / Vrod * dVrod * (1 - p.γ) +
+             p.Rₛ * u.Tr / Vrod / u.Pr * valveFlow(-p.command, u.Pr, u.Tr, u.Tr*(p.γ-1), p.Pin, p.Tin, (p.γ*p.Tin - u.Tr), p.Patm, p.Tatm, (p.γ*p.Tatm - u.Tr), p.Amax, p.Vdb, p.valve[1]) +
+             (p.γ - 1) * u.Tr / u.Pr / Vrod * p.λ * ((p.L-u.x) * Cpiston) * (p.Ta - u.Tr))
+    du
+end
+
+
 function makeProblem()
+    γ = 1.4 # = cₚ / cᵥ for air
+    Rₛ = 287.058u"J/kg/K"   # Specific gas constant for dry air, R / (molar mass), [J/(kg K)]
+    Patmospheric = 101325.0u"Pa"  # Pa
+
     Ta = 300.0u"K"     # K ambient temperature
-    Pa = 101325.0u"Pa" # Pa atmospheric pressure
     Rp = 0.025u"m"     # m piston radius
-    Ap = π*Rp^2        # m^2 piston area
     rod_r = 0.8        # unitless area ratio of piston to rod
     L = 0.10u"m"       # m cylinder length
 
     p = [
         L,                #L        m
-        Ap,               #Apiston  m^2
-        π * 2 * Rp,       #Cpiston  m
+        Rp,               #Apiston  m^2
         rod_r,            #r
         bestDeadband,     #Vdb      unitless command deadband
         25e-6u"m^2",      #Amax     m^2
@@ -77,18 +112,20 @@ function makeProblem()
         1.0u"kg*m/s^2",   #σd       kg m / s^2 dynamic friction
         0.001u"m/s",      #vmin     m/s minimum speed for static friction
         900e3u"Pa",       #Pin      Pa ≈ ~135psia valve inlet pressure
-        Pa,               #Patm     Pa
+        Patmospheric,     #Patm     Pa
         300.0u"K",        #Tatm     K
         0.0,              #valve command value
         bestValve,
+        γ,
+        Rₛ
     ]
 
     u0 = ArrayPartition(
         [L / 2],     # x   m
         [0.0u"m/s"], # v   m/s
-        [Pa],        # Pb  Pa
+        [Patmospheric],        # Pb  Pa
         [Ta],       # Tb  K
-        [Pa],       # Pr  Pa
+        [Patmospheric],       # Pr  Pa
         [Ta],       # Tr  K
     )
 
@@ -96,6 +133,52 @@ function makeProblem()
 
     ODEProblem(cylinder!, u0, tspan, p)
 end
+
+function makeCompProblem()
+
+    γ = 1.4 # = cₚ / cᵥ for air
+    Rₛ = 287.058u"J/kg/K"   # Specific gas constant for dry air, R / (molar mass), [J/(kg K)]
+
+    Patmospheric = 101325.0u"Pa"  # Pa
+
+    Ta = 300.0u"K"     # K ambient temperature
+
+    p = ComponentVector(
+        L = 0.10u"m",          # m cylinder length
+        Rp = 0.025u"m",        # m piston radius
+        r = 0.8,               # unitless area ratio of piston to rod
+        Vdb=bestDeadband,      #Vdb      unitless command deadband
+        Amax=25e-6u"m^2",      #Amax     m^2
+        Ta = Ta,               # K ambient temperature
+        λ = 0.0u"W/K/m^2",     #λ        heat transfer coefficient
+        Mass = 1.00u"kg",      #Mass     kg piston + rod + load mass
+        Tin = 273.0u"K",       #Tin      K valve inlet temperature
+        σs = 10.0u"kg*m/s^2",  #σs       kg m / s^2 static friction
+        σd = 1.0u"kg*m/s^2",   #σd       kg m / s^2 dynamic friction
+        vmin = 0.001u"m/s",    #vmin     m/s minimum speed for static friction
+        Pin = 900e3u"Pa",      #Pin      Pa ≈ ~135psia valve inlet pressure
+        Patm = Patmospheric,   #Patm     Pa
+        Tatm = 300.0u"K",      #Tatm     K
+        command = 0.0,         #valve command value
+        valve = (bestValve,),
+        γ = γ,
+        Rₛ = Rₛ
+    )
+
+    u0 = ComponentVector(
+        x = p.L / 2,     # x   m
+        v = 0.0u"m/s", # v   m/s
+        Pb = Patmospheric,        # Pb  Pa
+        Tb = Ta,       # Tb  K
+        Pr = Patmospheric,       # Pr  Pa
+        Tr = Ta,       # Tr  K
+    )
+
+    tspan = (0.0u"s", 1.0u"s")
+
+    ODEProblem(compcylinder!, u0, tspan, p)
+end
+
 
 struct Pulse
     start::Unitful.Time
@@ -125,11 +208,12 @@ callback(p::Pulse) = PresetTimeCallback(discontinuities(pulse), affect(p))
 
 function solveProblem(prob, pulse)
     cb = callback(pulse)
-    solve(prob, d_discontinuities=discontinuities(pulse), callback=cb, abstol=1e-10, reltol=1e-10)
+    solve(prob, d_discontinuities=discontinuities(pulse), callback=cb; hints=[:stiff])
 end
 
 function solveProblem(prob)
-    solve(prob, abstol=1e-10, reltol=1e-10)
+    #solve(prob; maxiters=1e8, dt=1e-4u"s") # , alg_hints=[:stiff])
+    solve(prob; maxiters=1e9, alg_hints=[:stiff])
 end
 
 function stepProblem(prob, u, t; dt=1e-3u"s")
