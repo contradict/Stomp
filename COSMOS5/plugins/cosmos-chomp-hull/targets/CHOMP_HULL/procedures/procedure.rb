282a7393bc6b168fea47246b70dd69b47bc96487
Matthew Scot <mjs@scottopia.net>
2022-04-21 20:37:45 -0700

    remove duplicate COSMOS5 plugins directory

--- COSMOS5/plugins/cosmos-chomp-hull/targets/CHOMP_HULL/procedures/procedure.rb
+++
@@ -1,3 +0,0 @@
-# Script Runner test script
-cmd("CHOMP_HULL EXAMPLE")
-wait_check("CHOMP_HULL STATUS BOOL == 'FALSE'", 5)
--- COSMOS5/plugins/cosmos-chomp-turret/targets/CHOMP_TURRET/procedures/procedure.rb
+++
@@ -1,3 +0,0 @@
-# Script Runner test script
-cmd("CHOMP_TURRET EXAMPLE")
-wait_check("CHOMP_TURRET STATUS BOOL == 'FALSE'", 5)
